13b98ad2957e319bd63ba135a952340141a91e19
AngelOrtiz <oiba.angel.ortiz@gmail.com>
2014-11-04 17:56:09 -0600

    04-11-2014 salida

--- sites/all/themes/AngelO_theme/templates/page.tpl.php
+++ sites/all/themes/AngelO_theme/templates/page.tpl.php
@@ -94,6 +94,9 @@
     <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
       <div class="navbar-collapse collapse">
         <nav role="navigation">
+           <?php if (!empty($page['navigation'])): ?>
+            <?php print render($page['navigation']); ?>
+          <?php endif; ?>
            <?php if (!empty($secondary_nav)): ?>
             <?php print render($secondary_nav); ?>
           <?php endif; ?>
@@ -106,9 +109,7 @@
       </a>
       <?php endif; ?>
          
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
+         
         </nav>
       </div>
     <?php endif; ?>
